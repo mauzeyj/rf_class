37f114c446822db2e7436e80900818db35475198
Jason Mauzey <jmauzey@clearwateranalytics.com>
2019-01-16 12:06:48 -0700

    added incremental_mean and egreedy_exploration

--- simple_21/monte_carlo.py
+++ simple_21/monte_carlo.py
@@ -4,12 +4,29 @@ import numpy as np
 from simple_21.environment import step
 
 states = []
+actions = []
 
 
-def step_size(states, actions):
-        return 1 / states
+def incremental_mean(x, u, k):
+    """
+    calculates incremental mean
+    :param x: current reward
+    :param u: mean of existing rewards
+    :param k: number of times state action pair has been seen
+    :return: new state action value
+    """
+    return u + ((1 / k) * (x - u))
 
 
+# %%
+def egreedy_exploration(k, k_fixed=100):
+    return k_fixed / (k_fixed + k)
+
+
+# %%
+
+# does state exist
+# actions list should have (stick, reward, evaluations), (hit, reward, evaluations)
 for x in range(20):
         state = [0, 0, 'other', np.nan]
         state = step(state)
@@ -18,8 +35,8 @@ for x in range(20):
                         action = 'hit'
                 else:
                         action = 'stick'
-                print(action)
                 state[2] = action
                 state = step(state)
-                states.append(state.append(action))
+                states.append(state)
+                actions.append(action)
                 print(state)
