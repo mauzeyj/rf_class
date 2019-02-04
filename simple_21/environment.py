2114b2ac28e3f8a43d7fb60aa828ec9a16c72d8e
Jason Mauzey <jmauzey@clearwateranalytics.com>
2019-02-04 15:45:14 -0700

    kerasrl and environment working together.

--- simple_21/environment.py
+++ simple_21/environment.py
@@ -1,14 +1,22 @@
 import numpy as np
+from gym import spaces
+from gym.utils import seeding
 
 
 class twenty_one():
     def __init__(self):
-        self.action_space = [1, 0]
+        self.action_space = spaces.Discrete(2)
+        self.observation_space = spaces.Box(np.array([0, -10]), np.array([31, 31]), dtype=np.float32)
         self.state = None
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
     def reset(self):
         self.state = np.array(
-            [twenty_one.card(self), twenty_one.card(self)]), np.nan, False, {}  # dealer player reward done info
+            [twenty_one.card(self), twenty_one.card(self)])  # dealer player reward done info
+        return self.state
 
     def card(self):
         return np.random.randint(1, 11)
@@ -28,9 +36,10 @@ class twenty_one():
         return c
 
     def step(self, action):
+        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
         state = self.state
-        dealer, player = state[0]
-        reward = None
+        dealer, player = state
+        reward = 0
         done = False
         if action == 1:
             player = player + twenty_one.draw(self)
@@ -52,5 +61,5 @@ class twenty_one():
             elif player > dealer:
                 reward = 1
             done = True
-        self.state = np.array([dealer, player]), reward, done, {}
-        return np.array([dealer, player]), reward, done, {}
+        self.state = np.array([dealer, player])
+        return np.array(self.state), reward, done, {}
